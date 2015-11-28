64644d927952804fe6fa7174c6a6306a4338439f
Felix Palta <fpalta90@gmail.com>
2015-11-28 15:12:16 +0300

    Add requests to LedRqHandler

--- server/ledrqhandler.cpp
+++ server/ledrqhandler.cpp
@@ -7,15 +7,82 @@ LedRqHandler::LedRqHandler(std::shared_ptr<IRgbLed> led)
 
 std::string LedRqHandler::process_request(RqType rq_type, const std::string& params)
 {
-    switch (rq_type)
-    {
-    case RqType::LED_GET_COLOR:
-        break;
-    case RqType::LED_SET_COLOR:
-        break;
-    default:
-        break;
+    std::string retval;
 
+    try
+    {
+        switch (rq_type)
+        {
+        case RqType::LED_GET_COLOR:
+            {
+                auto color = m_rgb_led->get_color();
+                retval = get_ok_with_result(get_color_name(color));
+            }
+            break;
+        case RqType::LED_GET_RATE:
+            {
+                auto rate = m_rgb_led->get_rate();
+                retval = get_ok_with_result(std::to_string(rate));
+            }
+            break;
+        case RqType::LED_GET_STATUS:
+            {
+                auto state = m_rgb_led->get_state();
+                retval = get_ok_with_result(state ? "on" : "off");
+            }
+            break;
+        default:
+            break;
+        }
     }
-    return "nope";
+    catch (IRgbLed::IRgbLedInternalException&)
+    {
+        // TODO: Add internal exception logging.
+        retval = get_failed_string();
+    }
+
+    return retval;
+}
+
+
+std::string LedRqHandler::get_failed_string()
+{
+    return "FAILED\n";
+}
+
+std::string LedRqHandler::get_ok_string()
+{
+    return "OK\n";
+}
+
+std::string LedRqHandler::get_ok_with_result(const std::string& result)
+{
+    return std::string("OK " + result + '\n');
+}
+
+std::string LedRqHandler::get_color_name(IRgbLed::Color color)
+{
+    switch (color)
+    {
+    case IRgbLed::Color::RED:
+        return "red";
+    case IRgbLed::Color::BLUE:
+        return "blue";
+    case IRgbLed::Color::GREEN:
+        return "green";
+    default:
+        return "";
+    }
+}
+
+IRgbLed::Color LedRqHandler::get_color_value(const std::string &s)
+{
+    if (s == "red")
+        return IRgbLed::Color::RED;
+    if (s == "green")
+        return IRgbLed::Color::GREEN;
+    if (s == "blue")
+        return IRgbLed::Color::BLUE;
+
+    return IRgbLed::Color::INVALID_COLOR;
 }
--- server/ledrqhandler.h
+++ server/ledrqhandler.h
@@ -14,6 +14,12 @@ public:
 
 private:
     std::shared_ptr<IRgbLed> m_rgb_led;
+
+    static std::string get_ok_with_result(const std::string &result);
+    static std::string get_ok_string();
+    static std::string get_failed_string();
+    static std::string get_color_name(IRgbLed::Color color);
+    static IRgbLed::Color get_color_value(const std::string &s);
 };
 
 #endif // LEDRQHANDLER_H
